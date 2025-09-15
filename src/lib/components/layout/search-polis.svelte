7156bda7baf1f5cef64d4b935f6846513cd84aba
Wiscaksono <wwicaksono96@gmail.com>
2025-09-16 02:25:58 +0700

    kuy

--- src/lib/components/layout/search-polis.svelte
+++ src/lib/components/layout/search-polis.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-	import { Command, Dialog } from 'bits-ui';
-
-	import { cn } from '$lib';
+	import { Dialog } from 'bits-ui';
 	import { Search } from '@lucide/svelte';
 	import { slide } from 'svelte/transition';
 
+	import { cn } from '$lib';
+
 	let { isSidebarOpen }: { isSidebarOpen: boolean } = $props();
 
+	let searchTerm = $state('');
 	let dialogOpen = $state(false);
 
 	function handleKeydown(e: KeyboardEvent) {
@@ -28,66 +29,24 @@
 	>
 		<Search class={cn(isSidebarOpen ? 'size-4' : 'size-5', 'shrink-0 transition-all')} />
 		{#if isSidebarOpen}
-			<span
-				class="flex-1 text-left text-sm leading-[12px] whitespace-nowrap"
-				transition:slide={{ axis: 'x', duration: 150 }}
-			>
-				Search Polis...
-			</span>
-			<span
-				class="block rounded-sm bg-[#D9D9D9] p-1 text-[10px] leading-[6px] whitespace-nowrap"
-				transition:slide={{ axis: 'x', duration: 150 }}
-			>
-				⌘ + P
-			</span>
+			<span class="flex-1 text-left text-sm leading-[12px] whitespace-nowrap" transition:slide={{ axis: 'x', duration: 150 }}> Search Polis... </span>
+			<span class="block rounded-sm bg-[#D9D9D9] p-1 text-[10px] leading-[6px] whitespace-nowrap" transition:slide={{ axis: 'x', duration: 150 }}> ⌘ + P </span>
 		{/if}
 	</Dialog.Trigger>
 
 	<Dialog.Portal>
 		<Dialog.Overlay
-			class="fixed inset-0 z-50 bg-black/50 opacity-0 transition-all data-[state=open]:opacity-100"
+			class="fixed inset-0 bg-black/80 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:animate-in data-[state=open]:fade-in-0"
 		/>
 		<Dialog.Content
-			class="fixed top-1/2 left-1/2 z-50 w-full max-w-[94%] -translate-x-1/2 -translate-y-1/2 outline-hidden sm:max-w-[490px] md:w-full"
+			class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95"
 		>
 			<Dialog.Title class="sr-only">Search Polis</Dialog.Title>
-			<Dialog.Description class="sr-only">
-				Search for policies, and other resources.
-			</Dialog.Description>
-			<Command.Root class="bg-white">
-				<Command.Input
-					class="focus-override h-input bg-background placeholder:text-foreground-alt/50 inline-flex truncate rounded-xl px-4 text-sm transition-colors focus:ring-0 focus:outline-hidden"
-					placeholder="Search for something..."
-				/>
-				<Command.List class="max-h-[280px] overflow-x-hidden overflow-y-auto px-2 pb-2">
-					<Command.Viewport>
-						<Command.Empty
-							class="text-muted-foreground flex w-full items-center justify-center pt-8 pb-6 text-sm"
-						>
-							No results found.
-						</Command.Empty>
-						<Command.Group>
-							<Command.GroupHeading class="text-muted-foreground px-3 pt-4 pb-2 text-xs">
-								Suggestions
-							</Command.GroupHeading>
-							<Command.GroupItems>
-								<Command.Item
-									class="rounded-button data-selected:bg-muted flex h-10 cursor-pointer items-center gap-2 px-3 py-2.5 text-sm capitalize outline-hidden select-none"
-									keywords={['getting started', 'tutorial']}
-								>
-									Introduction
-								</Command.Item>
-								<Command.Item
-									class="rounded-button data-selected:bg-muted flex h-10 cursor-pointer items-center gap-2 px-3 py-2.5 text-sm capitalize outline-hidden select-none"
-									keywords={['child', 'custom element', 'snippets']}
-								>
-									Delegation
-								</Command.Item>
-							</Command.GroupItems>
-						</Command.Group>
-					</Command.Viewport>
-				</Command.List>
-			</Command.Root>
+			<Dialog.Description class="sr-only">Search for policies, and other resources.</Dialog.Description>
+			<div class="p-5">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil incidunt debitis nobis a hic, fugit provident magni, velit delectus molestiae aspernatur
+				ipsa minus nam, earum minima culpa iusto eos nisi.
+			</div>
 		</Dialog.Content>
 	</Dialog.Portal>
 </Dialog.Root>
--- src/lib/queries/index.ts
+++ src/lib/queries/index.ts
@@ -1,3 +1,17 @@
+import { api } from '$lib/api';
+import { queryOptions } from '@tanstack/svelte-query';
+
 export * from './login';
 export * from './workbasket';
 export * from './data-polis';
+
+export const searchPolisQuery = (search = '') => {
+	return queryOptions({
+		queryKey: ['search-polis', search],
+		queryFn: async () => {
+			const { data } = await api.get(`/polaris/api-business-polaris/major/workbasket/search-polis?search=${search}`);
+			return data;
+		},
+		enabled: !!search
+	});
+};
