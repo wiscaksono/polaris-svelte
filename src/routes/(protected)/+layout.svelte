74973338bc55c17354865d5b58c97414697b2f26
Wiscaksono <wwicaksono96@gmail.com>
2025-09-17 15:16:52 +0700

    unused code

--- src/routes/(protected)/+layout.svelte
+++ src/routes/(protected)/+layout.svelte
@@ -1,16 +1,9 @@
 <script lang="ts">
 	import { Tooltip } from 'bits-ui';
 
-	import { api } from '$lib/api';
-	import { onMount } from 'svelte';
-
 	import Layout from '$lib/components/layout/layout.svelte';
 
-	let { children, data } = $props();
-
-	onMount(() => {
-		if (data.token) api.setToken(data.token);
-	});
+	let { children } = $props();
 </script>
 
 <Tooltip.Provider delayDuration={100}>
