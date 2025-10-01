586be158b8fbf34a2bfba34a4ebb65d52de56f8a
Wiscaksono <wwicaksono96@gmail.com>
2025-10-01 17:22:01 +0700

    enable svelte inspector

--- svelte.config.js
+++ svelte.config.js
@@ -10,14 +10,20 @@ const config = {
 	kit: {
 		adapter: process.env.VERCEL ? adapterVercel() : adapterNode(),
 		experimental: { remoteFunctions: true }
+	},
+	vitePlugin: {
+		inspector: {
+			toggleKeyCombo: 'meta-shift',
+			showToggleButton: 'always',
+			toggleButtonPos: 'bottom-right',
+			navKeys: {
+				parent: "ArrowUp",
+				child: "ArrowDown",
+				sibling: "ArrowRight",
+				prev: "ArrowLeft",
+			}
+		}
 	}
-	// vitePlugin: {
-	// 	inspector: {
-	// 		toggleKeyCombo: 'meta-shift',
-	// 		showToggleButton: 'always',
-	// 		toggleButtonPos: 'bottom-right'
-	// 	}
-	// }
 };
 
 export default config;
