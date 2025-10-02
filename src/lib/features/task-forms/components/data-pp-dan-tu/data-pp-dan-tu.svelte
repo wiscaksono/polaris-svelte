dbc3deb483f4c692a9cde8dd07c01c6cf83a06b3
Wiscaksono <wwicaksono96@gmail.com>
2025-10-02 11:33:09 +0700

    done kah step 2

--- src/lib/features/task-forms/components/data-pp-dan-tu/data-pp-dan-tu.svelte
+++ src/lib/features/task-forms/components/data-pp-dan-tu/data-pp-dan-tu.svelte
@@ -4,10 +4,17 @@
 	import { dataPPdanTUQueries } from './query';
 	import { getTaskFormContext } from '../../context';
 	import PemegangPolis from './pemegang-polis';
+	import * as Alert from '$lib/components/ui/alert/index.js';
+	import { CircleAlert } from '@lucide/svelte';
 
 	const { taskFormParams } = getTaskFormContext();
 
 	const query = createQuery(() => dataPPdanTUQueries.get({ caseId: taskFormParams.case_id, regSpaj: taskFormParams.reg_spaj }));
 </script>
+
+<Alert.Root class="mb-5" variant={query.data?.ppDanTuBerbeda ? 'destructive' : 'info'}>
+	<CircleAlert />
+	<Alert.Title>Pemegang Polis dan Tertanggung Utama adalah orang yang {query.data?.ppDanTuBerbeda ? 'berbeda' : 'sama'}</Alert.Title>
+</Alert.Root>
 
 <PemegangPolis data={query.data?.pemegangPolis} />
