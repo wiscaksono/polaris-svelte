07cb68abcfafa66140a25185ae515464f91db474
Wiscaksono <wwicaksono96@gmail.com>
2025-10-02 11:35:12 +0700

    adjust margin alert

--- src/lib/features/task-forms/components/data-pp-dan-tu/data-pp-dan-tu.svelte
+++ src/lib/features/task-forms/components/data-pp-dan-tu/data-pp-dan-tu.svelte
@@ -12,7 +12,7 @@
 	const query = createQuery(() => dataPPdanTUQueries.get({ caseId: taskFormParams.case_id, regSpaj: taskFormParams.reg_spaj }));
 </script>
 
-<Alert.Root class="mb-5" variant={query.data?.ppDanTuBerbeda ? 'destructive' : 'info'}>
+<Alert.Root class="mb-3.5" variant={query.data?.ppDanTuBerbeda ? 'destructive' : 'info'}>
 	<CircleAlert />
 	<Alert.Title>Pemegang Polis dan Tertanggung Utama adalah orang yang {query.data?.ppDanTuBerbeda ? 'berbeda' : 'sama'}</Alert.Title>
 </Alert.Root>
