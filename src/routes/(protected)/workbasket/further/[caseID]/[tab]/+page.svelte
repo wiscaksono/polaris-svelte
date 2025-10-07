f67cca69924497b800aad50d1cbf88a1feff14e5
Wiscaksono <wwicaksono96@gmail.com>
2025-10-07 21:16:47 +0700

    fix errors

--- src/routes/(protected)/workbasket/further/[caseID]/[tab]/+page.svelte
+++ src/routes/(protected)/workbasket/further/[caseID]/[tab]/+page.svelte
@@ -1,18 +1,1 @@
-<script lang="ts">
-	import { setTaskFormContext } from '$lib/features/task-forms/context.svelte';
-	import type { PageProps } from './$types';
-
-	let { data }: PageProps = $props();
-
-	setTaskFormContext({ taskFormParams: data.taskFormParams });
-</script>
-
-{#if data.currentTaskFormTab}
-	{#await data.currentTaskFormTab.component}
-		<p>Loading...</p>
-	{:then { default: Component }}
-		<Component taskFormParams={data.taskFormParams} />
-	{/await}
-{:else}
-	<div>Not found</div>
-{/if}
+TODO
--- src/routes/(protected)/workbasket/new-submission/[caseID]/[tab]/+page.svelte
+++ src/routes/(protected)/workbasket/new-submission/[caseID]/[tab]/+page.svelte
@@ -1,17 +1,14 @@
 <script lang="ts">
-	import { getTaskFormContext } from '$lib/features/task-forms/context.svelte';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
-
-	$inspect(getTaskFormContext());
 </script>
 
 {#if data.currentTaskFormTab}
 	{#await data.currentTaskFormTab.component}
 		<p>Loading...</p>
 	{:then { default: Component }}
-		<Component taskFormParams={data.taskFormParams} />
+		<Component />
 	{/await}
 {:else}
 	<div>Not found</div>
