06450a4898012b284d3299d983d46966f43f8683
Wiscaksono <wwicaksono96@gmail.com>
2025-10-08 23:38:22 +0700

    feat: hide escalation on reunderwritting

--- src/lib/features/task-forms/components/actions/eskalasi-ke-tl/eskalasi-ke-tl.svelte
+++ src/lib/features/task-forms/components/actions/eskalasi-ke-tl/eskalasi-ke-tl.svelte
@@ -16,7 +16,7 @@
 	let submitButton: HTMLButtonElement;
 
 	// const queryClient = useQueryClient();
-	const { taskFormParams } = getTaskFormContext();
+	const { taskFormParams, isReUnderwriting } = getTaskFormContext();
 	const mutation = createMutation(() => actionQueries.insertEscalationHistory());
 
 	function handleSubmit(e: Event) {
@@ -33,43 +33,45 @@
 	}
 </script>
 
-<Tooltip.Root>
-	<Dialog.Root bind:open>
-		<Dialog.Trigger>
-			{#snippet child({ props })}
-				<Button {...props} class="!pl-2.5">
-					<ArrowUp />
-					Eskalasi Ke TL
-				</Button>
-			{/snippet}
-		</Dialog.Trigger>
-		<Tooltip.Content>
-			<p>Jika tidak ada gantungan yang diinginkan, SPM dan Decision</p>
-		</Tooltip.Content>
-		<Dialog.Content>
-			<Dialog.Header>
-				<Dialog.Title>Eskalasi Ke TL</Dialog.Title>
-				<Dialog.Description>
-					Submit this case to the Team Leader (TL) for further review or approval if you need a decision, follow-up, or confirmation.
-				</Dialog.Description>
-			</Dialog.Header>
+{#if isReUnderwriting.data}
+	<Tooltip.Root>
+		<Dialog.Root bind:open>
+			<Dialog.Trigger>
+				{#snippet child({ props })}
+					<Button {...props} class="!pl-2.5">
+						<ArrowUp />
+						Eskalasi Ke TL
+					</Button>
+				{/snippet}
+			</Dialog.Trigger>
+			<Tooltip.Content>
+				<p>Jika tidak ada gantungan yang diinginkan, SPM dan Decision</p>
+			</Tooltip.Content>
+			<Dialog.Content>
+				<Dialog.Header>
+					<Dialog.Title>Eskalasi Ke TL</Dialog.Title>
+					<Dialog.Description>
+						Submit this case to the Team Leader (TL) for further review or approval if you need a decision, follow-up, or confirmation.
+					</Dialog.Description>
+				</Dialog.Header>
 
-			<form onsubmit={handleSubmit}>
-				<div class="space-y-2">
-					<Label for="note">Note</Label>
-					<Textarea id="note" placeholder="Note" bind:value={remarks} />
-				</div>
-				<button type="submit" bind:this={submitButton} class="hidden">submit</button>
-			</form>
+				<form onsubmit={handleSubmit}>
+					<div class="space-y-2">
+						<Label for="note">Note</Label>
+						<Textarea id="note" placeholder="Note" bind:value={remarks} />
+					</div>
+					<button type="submit" bind:this={submitButton} class="hidden">submit</button>
+				</form>
 
-			<Dialog.Footer>
-				<Button onclick={() => submitButton.click()} disabled={!remarks || mutation.isPending}>
-					Confirm
-					{#if mutation.isPending}
-						<LoaderCircle class="h-4 w-4 animate-spin" data-testid="loading-spinner" />
-					{/if}
-				</Button>
-			</Dialog.Footer>
-		</Dialog.Content>
-	</Dialog.Root>
-</Tooltip.Root>
+				<Dialog.Footer>
+					<Button onclick={() => submitButton.click()} disabled={!remarks || mutation.isPending}>
+						Confirm
+						{#if mutation.isPending}
+							<LoaderCircle class="h-4 w-4 animate-spin" data-testid="loading-spinner" />
+						{/if}
+					</Button>
+				</Dialog.Footer>
+			</Dialog.Content>
+		</Dialog.Root>
+	</Tooltip.Root>
+{/if}
